9ba1f7eb23ed7e03c915072378a5f606eadd0dbf
manasseh liassidji <manassehl9@gmail.com>
2022-08-08 14:57:25 +0100

    chore: add modals for reinvite and delete user

--- src/pages/User.js
+++ src/pages/User.js
@@ -16,6 +16,8 @@ import { withAuth } from "../hoc/withAuth";
 const User = () => {
     const dispatch = useDispatch();
     const [showModal, setShowModal] = useState(false);
+    const [showInviteModal, setShowInviteModal] = useState(false);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
     const [toggle, setToggle] = useState(true);
     const { register, handleSubmit, setValue, formState: { errors } } = useForm();
     let myArr = [
@@ -209,6 +211,91 @@ const User = () => {
                         <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
                     </>
                 ) : null}
+
+                {showInviteModal ? (
+                    <>
+                        <div
+                            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        >
+                            <div className="relative w-auto my-6 mx-auto max-w-xl">
+                                {/*content*/}
+                                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+
+                                    {/*body*/}
+                                    <form className='space-y-6 py-6' onSubmit={handleSubmit(onSubmit)}>
+                                        <div className="relative px-8 flex-auto">
+                                            <div className="relative w-12/12">
+                                                <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                                    Do you want to reinvite user?
+                                                </p>
+                                            </div>
+                                        </div>
+                                        {/*footer*/}
+                                        <div className="flex items-center justify-center p-6 mt-5 rounded-b">
+                                            <button
+                                                onClick={() => setShowInviteModal(false)}
+                                                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 mb-1 mr-2"
+                                                type="submit"
+                                            >
+                                                No
+                                            </button>
+                                            <button
+                                                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 mb-1"
+                                                type="submit"
+                                            >
+                                                Yes
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                    </>
+                ) : null}
+
+
+                {showDeleteModal ? (
+                    <>
+                        <div
+                            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        >
+                            <div className="relative w-auto my-6 mx-auto max-w-xl">
+                                {/*content*/}
+                                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+
+                                    {/*body*/}
+                                    <form className='space-y-6 py-6' onSubmit={handleSubmit(onSubmit)}>
+                                        <div className="relative px-8 flex-auto">
+                                            <div className="relative w-12/12">
+                                                <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                                                    Are you sure you want to delete user?
+                                                </p>
+                                            </div>
+                                        </div>
+                                        {/*footer*/}
+                                        <div className="flex items-center justify-center p-6 mt-5 rounded-b">
+                                            <button
+                                                onClick={() => setShowDeleteModal(false)}
+                                                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 mb-1 mr-2"
+                                                type="submit"
+                                            >
+                                                No
+                                            </button>
+                                            <button
+                                                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 mb-1"
+                                                type="submit"
+                                            >
+                                                Yes
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                    </>
+                ) : null}
                 {/* End Modal */}
 
                 {/* Header */}
@@ -282,7 +369,7 @@ const User = () => {
                                             <td className=" border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                                                 <div className="flex justify-start space-x-5">
 
-                                                    <div className="has-tooltip">
+                                                    <div className="has-tooltip" onClick={() => setShowInviteModal(true)}>
                                                         <span className="tooltip rounded shadow-lg p-2 bg-gray-100 text-green-700 -mt-8">reinvite user</span>
                                                         <FontAwesomeIcon
                                                             icon={faRedo}
@@ -290,7 +377,7 @@ const User = () => {
                                                         />
 
                                                     </div>
-                                                    <div className="has-tooltip">
+                                                    <div className="has-tooltip" onClick={() => setShowDeleteModal(true)}>
                                                         <span className="tooltip rounded shadow-lg p-2 bg-gray-100 text-red-500 -mt-8">delete user</span>
                                                         <FontAwesomeIcon
                                                             icon={faTrashAlt}
