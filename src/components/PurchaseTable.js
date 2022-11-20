e7fb1c50df94252830828457212cfc3a4c5ea052
Manasseh Abiodun <manieabiodun@gmail.com>
2022-11-20 16:03:01 +0100

    create new component for sales, purchase, invoice, bills

---
+++ src/components/PurchaseTable.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import CurrencyFormat from 'react-currency-format';
+
+const PurchaseTable = () => {
+  let isGeneratingReport = useSelector(
+    (state) => state.forecast.isGeneratingReport
+  );
+  let purchases = useSelector((state) => state.forecast.purchases);
+
+  return (
+    <>
+      <div className="w-full xl:w-6/12 px-4">
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+          <h2 className="text-black text-sm font-semibold px-7">
+            Purchase Orders
+          </h2>
+          <div className="table-wrp block max-h-96  overflow-x-auto">
+            {/* Projects table */}
+            <table className="w-full">
+              <thead className="bg-white border-blueGray-100 sticky top-0">
+                <tr>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-bold text-left">
+                    #
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap  font-bold text-left">
+                    Purchase ID
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap  font-semibold text-left">
+                    Vendor Name
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Balance Due
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Currency
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Status
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Delivery Date
+                  </th>
+                </tr>
+              </thead>
+              {!isGeneratingReport && (
+                <tbody className="border-blueGray-100 ">
+                  {purchases.map((purchase, i) => (
+                    <tr key={purchase.id}>
+                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                        {i + 1}
+                      </th>
+                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                        {purchase.refrenceNumber}
+                      </th>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        {purchase.purchaseOrderNumber}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <CurrencyFormat
+                          value={parseFloat(purchase.balance)}
+                          displayType={'text'}
+                          thousandSeparator={true}
+                          decimalScale={2}
+                        />
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                        {purchase.currencyCode}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                        {purchase.status}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                        {purchase.deliveryDate}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default PurchaseTable;
---
+++ src/components/SaleTable.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import CurrencyFormat from 'react-currency-format';
+
+const SaleTable = () => {
+  let isGeneratingReport = useSelector(
+    (state) => state.forecast.isGeneratingReport
+  );
+  let sales = useSelector((state) => state.forecast.sales);
+
+  return (
+    <>
+      <div className="w-full xl:w-6/12 px-4">
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+          <h2 className="text-black text-sm font-semibold px-7">Sales</h2>
+          <div className="table-wrp block max-h-96  overflow-x-auto">
+            {/* Projects table */}
+            <table className="w-full">
+              <thead className="bg-white border-blueGray-100 sticky top-0">
+                <tr>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-bold text-left">
+                    #
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap  font-bold text-left">
+                    Sales ID
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap  font-semibold text-left">
+                    Customer Name
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Balance Due
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Currency
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Status
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Shipment Date
+                  </th>
+                </tr>
+              </thead>
+              {!isGeneratingReport && (
+                <tbody className="border-blueGray-100 ">
+                  {sales.map((sale, i) => (
+                    <tr key={sale.id}>
+                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                        {i + 1}
+                      </th>
+                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                        {sale.salesOrderNumber}
+                      </th>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        {sale.customerName}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <CurrencyFormat
+                          value={parseFloat(sale.balance)}
+                          displayType={'text'}
+                          thousandSeparator={true}
+                          decimalScale={2}
+                        />
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                        {sale.currencyCode}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                        {sale.status}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                        {sale.shipmentDate}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              )}
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default SaleTable;
