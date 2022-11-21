36dae2afbcaa79b2cb2898557e1d63428d662a30
Manasseh Abiodun <manieabiodun@gmail.com>
2022-11-21 16:50:22 +0100

    add purchase modal

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -37,6 +37,8 @@ const Dashboard = (props) => {
   let [saleDetail, setSaleDetail] = useState('');
   let [showBillDetailModal, setShowBillDetailModal] = useState(false);
   let [billDetail, setBillDetail] = useState('');
+  let [showPurchaseDetailModal, setShowPurchaseDetailModal] = useState(false);
+  let [purchaseDetail, setPurchaseDetail] = useState('');
   let [code, setCode] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -652,6 +654,181 @@ const Dashboard = (props) => {
                 <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
               </>
             ) : null}
+
+            {/* Purchase Modal */}
+            {showPurchaseDetailModal ? (
+              <>
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                  <div className="fixed w-auto my-6 mx-auto max-w-xl">
+                    {/*content*/}
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                      {/*header*/}
+                      <div className="flex items-start justify-around mt-5 rounded-t">
+                        <div className="flex-initial w-64 justify-center">
+                          <h3 className="text-red-700 font-medium text-lg mb-1 ">
+                            Purchase Detail
+                          </h3>
+                        </div>
+                        <button
+                          onClick={() => setShowPurchaseDetailModal(false)}
+                          type="button"
+                          className=" rounded-md p-1 inline-flex items-center justify-center text-red-400 hover:text-red-500 bg-red-100 hover:bg-red-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-red-500"
+                        >
+                          <span className="sr-only">Close menu</span>
+
+                          <svg
+                            className="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M6 18L18 6M6 6l12 12"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                      {/*body*/}
+                      <div className="relative px-8 flex-auto">
+                        <div className="relative w-12/12">
+                          <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                            Purchase detail for {purchaseDetail.purchaseOrderId}
+                          </p>
+
+                          <div className="mb-4 md:flex md:justify-between">
+                            <div className="w-full">
+                              <label className="block text-gray-700">
+                                Vendor Name
+                              </label>
+
+                              <input
+                                type="text"
+                                name="email"
+                                value={purchaseDetail.vendorName}
+                                style={{ transition: 'all .15s ease' }}
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-grey-200 focus:bg-white focus:outline-none"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="mb-4 md:flex md:justify-between">
+                            <div className="w-full">
+                              <label className="block text-gray-700">
+                                Reference Number
+                              </label>
+
+                              <input
+                                type="text"
+                                name="email"
+                                value={purchaseDetail.refrenceNumber}
+                                style={{ transition: 'all .15s ease' }}
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-grey-200 focus:bg-white focus:outline-none"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="mb-4 md:flex md:justify-between">
+                            <div className="mb-4 md:mr-2 md:mb-0">
+                              <label className="block text-gray-700">
+                                Purchase Order Number
+                              </label>
+
+                              <input
+                                type="text"
+                                name="email"
+                                value={purchaseDetail.purchaseOrderNumber}
+                                style={{ transition: 'all .15s ease' }}
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-grey-200 focus:bg-white focus:outline-none"
+                              />
+                            </div>
+                            <div className="mb-4 md:mr-2 md:mb-0">
+                              <label className="block text-gray-700">
+                                Status
+                              </label>
+
+                              <input
+                                type="text"
+                                name="email"
+                                value={purchaseDetail.status}
+                                style={{ transition: 'all .15s ease' }}
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-grey-200 focus:bg-white focus:outline-none"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="mb-4 md:flex md:justify-between">
+                            <div className="mb-4 md:mr-2 md:mb-0">
+                              <label className="block text-gray-700">
+                                Balance
+                              </label>
+
+                              <input
+                                type="text"
+                                name="email"
+                                value={purchaseDetail.balance}
+                                style={{ transition: 'all .15s ease' }}
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-grey-200 focus:bg-white focus:outline-none"
+                              />
+                            </div>
+                            <div className="mb-4 md:mr-2 md:mb-0">
+                              <label className="block text-gray-700">
+                                Currency
+                              </label>
+
+                              <input
+                                type="text"
+                                name="email"
+                                value={purchaseDetail.currencyCode}
+                                style={{ transition: 'all .15s ease' }}
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-grey-200 focus:bg-white focus:outline-none"
+                              />
+                            </div>
+                          </div>
+
+                          <div className="mb-4 md:flex md:justify-between">
+                            <div className="mb-4 md:mr-2 md:mb-0">
+                              <label className="block text-gray-700">
+                                Date created
+                              </label>
+
+                              <input
+                                type="text"
+                                name="email"
+                                value={purchaseDetail.date}
+                                style={{ transition: 'all .15s ease' }}
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-grey-200 focus:bg-white focus:outline-none"
+                              />
+                            </div>
+                            <div className="mb-4 md:mr-2 md:mb-0">
+                              <label className="block text-gray-700">
+                                Due Date
+                              </label>
+
+                              <input
+                                type="text"
+                                name="email"
+                                value={purchaseDetail.dueDate}
+                                style={{ transition: 'all .15s ease' }}
+                                className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-grey-200 focus:bg-white focus:outline-none"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      {/*footer*/}
+                      <div className="flex items-center justify-center p-6 mt-5 rounded-b"></div>
+                    </div>
+                  </div>
+                </div>
+                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+              </>
+            ) : null}
+
             {/* Header */}
             <div className="relative bg-pink-600 md:pt-32 pb-32 pt-12">
               <div className="px-4 md:px-10 mx-auto w-full">
@@ -876,7 +1053,10 @@ const Dashboard = (props) => {
                   setSaleDetail={setSaleDetail}
                 />
 
-                <PurchaseTable />
+                <PurchaseTable
+                  setShowPurchaseDetailModal={setShowPurchaseDetailModal}
+                  setPurchaseDetail={setPurchaseDetail}
+                />
               </div>
 
               <footer className="block py-4">
