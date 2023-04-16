050956cda1e93b3f2058f27030f35eddf402b337
manasseh abiodun <manieabiodun@gmail.com>
2023-04-16 15:06:23 +0100

    purchase and sales table implementation

--- src/components/PurchaseTable.js
+++ src/components/PurchaseTable.js
@@ -15,7 +15,7 @@ const PurchaseTable = (props) => {
 
   return (
     <>
-      <div className="w-full xl:w-6/12 px-4">
+      <div className="w-full xl:w-12/12 mb-12 xl:mb-0 px-4">
         <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
           <h2 className="text-black text-sm font-semibold px-7">
             Purchase Orders
@@ -35,16 +35,22 @@ const PurchaseTable = (props) => {
                     Vendor Name
                   </th>
                   <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Naira Balance
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Dollar Balance
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                     Balance Due
+                  </th>
+                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                    Delivery Date
                   </th>
                   <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                     Currency
                   </th>
                   <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                     Status
-                  </th>
-                  <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
-                    Delivery Date
                   </th>
                 </tr>
               </thead>
@@ -58,13 +64,46 @@ const PurchaseTable = (props) => {
                       <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
                         {i + 1}
                       </th>
-                      <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
-                        {purchase.refrenceNumber}
-                      </th>
                       <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                         {purchase.purchaseOrderNumber}
                       </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">
+                        {purchase.vendorName}
+                      </td>
                       <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        {purchase.currencyCode == 'NGN' ? (
+                          <>
+                            <span className="font-semibold text-sm">
+                              &#8358;
+                            </span>{' '}
+                            <CurrencyFormat
+                              value={parseFloat(purchase.balance)}
+                              displayType={'text'}
+                              thousandSeparator={true}
+                              decimalScale={2}
+                            />
+                          </>
+                        ) : null}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        {purchase.currencyCode == 'USD' ? (
+                          <>
+                            <span className="font-semibold text-sm">&#36;</span>{' '}
+                            <CurrencyFormat
+                              value={parseFloat(purchase.balance)}
+                              displayType={'text'}
+                              thousandSeparator={true}
+                              decimalScale={2}
+                            />
+                          </>
+                        ) : null}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        {purchase.currencyCode != 'USD' ? (
+                          <span className="font-semibold text-sm">&#8358;</span>
+                        ) : (
+                          <span className="font-semibold text-sm">&#36;</span>
+                        )}{' '}
                         <CurrencyFormat
                           value={parseFloat(purchase.balance)}
                           displayType={'text'}
@@ -74,15 +113,15 @@ const PurchaseTable = (props) => {
                       </td>
                       <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                         <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
+                        {purchase.deliveryDate}
+                      </td>
+                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                        <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
                         {purchase.currencyCode}
                       </td>
                       <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                         <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
                         {purchase.status}
-                      </td>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                        <i className="fas fa-arrow-up text-emerald-500 mr-4"></i>
-                        {purchase.deliveryDate}
                       </td>
                     </tr>
                   ))}
